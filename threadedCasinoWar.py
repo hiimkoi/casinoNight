c0abb67b3027b29a34322044ca8e4c4414fab3fa
Dan Menjivar <dan212edu@gmail.com>
2018-12-03 10:45:55 -0800

    started building the game card judging part of the game

--- threadedCasinoWar.py
+++ threadedCasinoWar.py
@@ -37,27 +37,27 @@ class Card:  # Class Card created for use in Deck of cards
     def __str__(self):  # redefining string method for use of printing card name
         return "%s of %s" % (self.rank, self.suit)
 
-    def getValue(self):
-        if not isinstance(self.rank, str):
-            return self.rank - 1
+    def getValue(self):  # method returns the integer value of a card for Casino War Game
+        if not isinstance(self.rank, str):  # string ranks are special cases
+            return self.rank - 1  # non string ranks get their numeric values - 1 because of ace
         else:
-            if self.rank == "Jack":
+            if self.rank == "Jack":  # all other card ranks need to be given special care
                 return 10
             elif self.rank == "Queen":
                 return 11
             elif self.rank == "King":
                 return 12
-            else: #case for Ace
+            else:  # case for Ace
                 return 13
 
 
-def makeDeck():  # method builds 6 deck of cards for playing a game of Casino War
+def makeDeck():  # function builds 6 deck of cards for playing a game of Casino War
     ranks = [_ for _ in range(2, 11)] + ["Jack", "Queen", "King", "Ace"]
     suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
 
     deck = []  # empty deck using list
-    for i in range(6):  # need 6 decks of cards
-        for rank in range(len(ranks)):  # each with their corresponding ranl
+    for i in range(6):  # need 6 decks of cards for a proper game of casino war
+        for rank in range(len(ranks)):  # each with their corresponding rank
             for suit in range(len(suits)):  # and suit
                 newCard = Card()
                 newCard.suit = suits[suit]
@@ -68,52 +68,67 @@ def makeDeck():  # method builds 6 deck of cards for playing a game of Casino Wa
 
 class Dealer:  # Dealer class contains the dealer's card
     def __init__(self):
-        self.card = None
+        self.card = None  # setter method takes care of this
+        self.houseWinnings = 0  # just for geeky fun keep track of the winnings siphoned from the players
 
-    def __str__(self):
+    def __str__(self):  # overwritten string RV for dealer class used in printing
         return "Dealer has %s" % self.card
 
-    def assignCard(self, card):
+    def assignCard(self, card):  # setter method assigns instance variable card
         self.card = card
 
-    #def play(self, card):
-
-
-
-
+    def plays(self, player):  # method performs the card judgment aka the actually game part of the game
+        playerValue = player.card.getValue()  # grab corresponding card values for judgement
+        dealerValue = self.card.getValue()
+        if dealerValue == playerValue:  # player and dealer ties
+            if player.goToWar():  # player decides to go to war
+                return False
+            else:  # player forfeits
+                player.money += player.bet / 2  # return half of player's bet to their money
+                player.bet = 0
+        elif dealerValue < playerValue:  # player wins
+            print("Player #%i wins $%.2f!" % (player.id, 2 * player.bet))
+            player.money += player.bet * 2  # player receives twice their bet
+            player.bet = 0
+        else:  # player lost
+            print("Player #%i loses $%.2f!" % (player.id, player.bet))
+            self.houseWinnings += player.bet  # nothing really happens but I keep track of the winnings for stats
+            player.bet = 0
 
 
 class Player:  # Player class contains a corresponding players card and how much money they have
 
     def __init__(self, id):
-        self.id = id
-        self.money = getMoney()
-        self.bet = 0
-        self.card = None
+        self.id = id  # verifying which player this is
+        self.money = getMoney()  # on construction receives random money wad
+        self.bet = 0  # tracks betting amount
+        self.card = None  # card player is assigned during game
 
-    def __str__(self):
+    def __str__(self):  # overwritten string RV for player class used in printing
         return "Player #%i has %s, and $%.2f" % (self.id, self.card, self.money)
 
-    def makeBet(self):
+    def makeBet(self):  # method randomly assigns a bet value and subtracts it from the players total money
         betAmount = random.randint(1, self.money + 1)
         self.money -= betAmount
         self.bet = betAmount
         print("Player #%i has made a $%.2f bet" % (self.id, self.bet))
 
-    def hasBusted(self):
+    def hasBusted(self):  # method checks to see if player has any money left to play or if player has busted
         if self.money <= 0:
             print("Player #%i has busted!" % self.id)
             return True
         else:
             return False
 
-    def assignCard(self, card):
+    def assignCard(self, card):  # setter method assigns card to player
         self.card = card
 
-def getMoney(): #method generates a random amount of money from $500 - $2000
+    def goToWar(self):  # method randomly decides for the player in the case of a tie, if player goes to war or forfeits
+        return random.choice([True, False])
+
+
+def getMoney():  # method generates a random amount of money from $500 - $2000
     return float(random.randint(500, 2001))
-
-
 
 
 def playGame():  # main method that plays the game
@@ -124,26 +139,24 @@ def playGame():  # main method that plays the game
     # round 1
     dealer = Dealer()
 
-    #print("Dealer has: %s" % dealer.card)
-
+    # print("Dealer has: %s" % dealer.card)
 
     # generate random money amount $500-2000
 
     # money = float(random.randint(500, 2001))
     # print(money)
 
-    #initialize 3 players
+    # initialize 3 players
 
     theHouse = 0
 
     players = []
     for i in range(3):
         players.append(Player(i + 1))
-        #print(players[i])
-
+        # print(players[i])
 
     numOfRounds = 1
-    while(len(players) > 1):
+    while (len(players) > 1):
 
         print("Start of round #%i" % numOfRounds)
         dealer.assignCard(deck.pop())
@@ -153,20 +166,13 @@ def playGame():  # main method that plays the game
             player.makeBet()
             player.assignCard(deck.pop())
             print(player)
-
-
-
+            dealer.plays(player)
 
             if player.hasBusted():
                 players.remove(player)
 
         print("End of round #%i\n" % numOfRounds)
         numOfRounds += 1
-
-
-
-
-
 
     # i = 1
     # for card in deck:
